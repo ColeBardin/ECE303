a056e7ead0ad4e62894d6a2a78e48fa81544c580
Cole Bardin <cab572@drexel.edu>
2024-04-20 20:39:39 -0400

    updated main

--- module3/src/main.cpp
+++ module3/src/main.cpp
@@ -6,17 +6,14 @@ DigitalPin p(11);
 void setup()
 {
     Serial.begin(9600);
-    DDRB = 0b00100000;
-    //p.set_pin(GPIO_OUTPUT);
+    p.set_pin(GPIO_OUTPUT);
+    p.write(0);
 }
 
 void loop()
 {
-    PORTB ^= 0b00100000;
-    /*
     p.write(1);
     p.write(0);
     delay(5);
-    */
 }
 
